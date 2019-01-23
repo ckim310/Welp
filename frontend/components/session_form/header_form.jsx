d583c0a8baa382ae2c9b5101ae3ceb19e5a54031
Christine Kim <ckim31093@gmail.com>
2019-01-23 16:43:07 -0500

    Change signup to include new header

---
+++ frontend/components/session_form/header_form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HeaderForm = (props) => {
+
+  return (
+    <div className="header-wrapper">
+      <h1 className="header">
+        <Link to="/">welp</Link>
+      </h1>
+    </div>  
+  )
+}
+
+export default HeaderForm;
