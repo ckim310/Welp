5c52eac4c52593e4deb9ff135a83004f195100ee
Christine Kim <ckim31093@gmail.com>
2019-01-23 10:59:18 -0500

    Ensure session util ajax requests are correctly routing

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -9,7 +9,7 @@ export const signup = (user) => {
 export const login = (user) => {
   return $.ajax({
     method: "POST",
-    url: "api/sessions",
+    url: "api/session",
     data: { user }
   });
 };
@@ -17,6 +17,6 @@ export const login = (user) => {
 export const logout = () => {
   return $.ajax({
     method: 'DELETE',
-    url: "api/sessions",
+    url: "api/session",
   });
 };
