781c800e6ef21a94dd70bc6253535748b12de64b
Christine Kim <ckim31093@gmail.com>
2019-01-24 16:15:14 -0500

    Add empty anchor tags under search bar

--- frontend/components/home_page/home_top.jsx
+++ frontend/components/home_page/home_top.jsx
@@ -13,9 +13,33 @@ const HomeTop = (props) => {
         </h1>
       </div>
 
-      <div className="home-inner-search">
+      <div className="home-inner-search-wrapper">
         <SearchForm />
+      </div>
 
+      <div className="home-inner-links-wrapper">
+        <div className="search-links">
+          <a className="restaurant" href="#">
+            <i class="fas fa-utensils"></i>
+            Restaurants
+          </a>
+
+          <a className="nightlife" href="#">
+            <i class="fas fa-cocktail"></i>
+            Nightlife
+          </a>
+
+          <a className="home-service" href="#">
+            <i class="fas fa-tools"></i>
+            Home Services
+          </a>
+
+          <a className="delivery" href="#">
+            <i class="fas fa-truck"></i>
+            Delivery
+          </a>
+
+        </div>
       </div>
 
     </div>
