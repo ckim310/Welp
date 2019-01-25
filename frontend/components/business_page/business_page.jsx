99d7c76becf0f5261a3a767235086ef35757afd8
Christine Kim <ckim31093@gmail.com>
2019-01-25 18:17:07 -0500

    Input business page top content

--- frontend/components/business_page/business_page.jsx
+++ frontend/components/business_page/business_page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { fetchBusiness } from '../../actions/business_actions';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class BusinessPage extends React.Component {
   constructor(props) {
@@ -29,6 +30,7 @@ class BusinessPage extends React.Component {
           
             <div className="top-container">
               <div className="top-header">
+
                 <div className="top-header-left">
                   <div className="top-header-title">
                     <h1 className="title-name">{business.name}</h1>
@@ -39,10 +41,66 @@ class BusinessPage extends React.Component {
                   </div>
 
                   <div className="top-header-avg-rating">
+                    <div className="rating-container">
+                      <div className="avg-rating-reviews">
+                        <div className="star-avg-rating">
+                          <i className="fas fa-star"></i>
+                          <i className="fas fa-star"></i>
+                          <i className="fas fa-star"></i>
+                          <i className="fas fa-star"></i>
+                          <i className="fas fa-star"></i>
+                        </div>
+
+                        <div className="reviews-count">
+                          Review Count (placeholder)
+                        </div>
+                      </div>
+
+                      <div className="rating-details">
+                        <Link to={`/businesses/${business.id}`}>
+                          <i className="fas fa-signal"></i>
+                          Details
+                        </Link>
+                      </div>
+                    </div>
+
+                    <div className="price-category">
+                      <div className="price">
+                        $$ •
+                      </div>
+
+                      <div className="business-category">
+                        Catgories!!!
+                      </div>
+                    </div>
                   </div>
                 </div>
 
                 <div className="top-header-right">
+                  <div className="business-page-actions">
+                    <a className="business-write-review" href="#">
+                      <i className="fas fa-star"></i>
+                      <div className="write-review-btn-text">Write a Review</div>
+                    </a>
+
+                    <div className="page-options">
+                      <Link to={`/businesses/${business.id}`}>
+                        <i className="fas fa-camera"></i>
+                        Add Photo
+                      </Link>
+
+                      <Link to={`/businesses/${business.id}`}>
+                        <i className="fas fa-share-square"></i>
+                        Share
+                      </Link>
+
+                      <div className="bookmark">
+                        <i className="fas fa-bookmark"></i>
+                        Save
+                      </div>
+
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
