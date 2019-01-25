66cc7d9d84b677572ff518ecdc277ac9e6eb424c
Christine Kim <ckim31093@gmail.com>
2019-01-25 10:19:00 -0500

    Add some styling to nav bar user dropdown to include user name and icon

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -13,28 +13,49 @@ class NavBar extends React.Component {
   }
 
   render() {
+    const userPic = <i className="fas fa-cookie-bite fa-2x"></i>;
+
     const display = this.props.currentUser ? (
       <div className="greeting">
         <div className="greeting-notif-wrapper">
           <div className="message-icon">
-            <a href="#">
-              <i className="fas fa-comment-alt"></i>
-            </a>
+            <button>
+              <i className="fas fa-comment-alt fa-2x"></i>
+            </button>
           </div>
 
           <div className="bell-icon">
-            <a href="#">
-              <i className="fas fa-bell"></i>
-            </a>
+            <button>
+              <i className="fas fa-bell fa-2x"></i>
+            </button>
           </div>
 
           <div className="user-dropdown">
             <button onClick={this.dropdownHandler}>
-              Hello {this.props.currentUser.first_name}<i className="fas fa-cookie-bite"></i>
+              {userPic}
             </button>
             <div id="dropdown" className="dropdown-content-container">
               <div className="dropdown-content">
-                <div className="dropdown-logout" onClick={this.props.logout}>Logout</div>
+                <div className="dropdown-user-container">
+                  <div className="user-pic">{userPic}</div>
+
+                  <div className="user-content-container">
+                    <div className="user-name">
+                      <div className="user-name_first">
+                        {this.props.currentUser.first_name}
+                      </div>
+
+                      <div className="user-name_last">
+                        {this.props.currentUser.last_name}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="dropdown-logout-container">
+                  <div className="dropdown-logout" onClick={this.props.logout}>Logout</div>
+                </div>
+
               </div>
             </div>
           </div>
@@ -55,7 +76,9 @@ class NavBar extends React.Component {
           <a href="https://github.com/ckim310" className="nav-bar-link" id="github">Github <i className="fab fa-github"></i></a>
           <a href="https://www.linkedin.com/in/christine-kim-46857544/" className="nav-bar-link" id="linkedin">LinkedIn <i className="fab fa-linkedin"></i></a>
         </div>
-        {display}
+        <div className="nav-bar-right">
+          {display}
+        </div>
       </div>
     )
   }
