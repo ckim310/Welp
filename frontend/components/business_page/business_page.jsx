aad11e05539441d0485d92ae7544d01412ffa4f0
Christine Kim <ckim31093@gmail.com>
2019-01-26 19:53:23 -0500

    Style business page header and separate into new file

--- frontend/components/business_page/business_page.jsx
+++ frontend/components/business_page/business_page.jsx
@@ -1,131 +1,19 @@
 import React from 'react';
-import { fetchBusiness, fetchBusinesses } from '../../actions/business_actions';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import BusinessPageHeaderContainer from './business_page_header_container';
 
 class BusinessPage extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  componentDidMount() {
-    return (
-      this.props.fetchBusiness(this.props.business.id)
-    );
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.match.params.businessId !== prevProps.match.params.businessId) {
-      return this.props.fetchBusiness(this.props.business.id);
-    }
-  }
-
   render() {
-    const { business } = this.props;
-    
-    return(
+
+    return (
       <div className="business-page-container">
-        <div className="business-page-wrapper">
-          <div className="top">
-          
-            <div className="top-container">
-              <div className="top-header">
-
-                <div className="top-header-left">
-                  <div className="top-header-title">
-                    <h1 className="title-name">{business.name}</h1>
-                    <div className="title-claim">
-                      <i className="fas fa-check-circle"></i>
-                       Claimed
-                    </div>
-                  </div>
-
-                  <div className="top-header-avg-rating">
-                    <div className="rating-container">
-                      <div className="avg-rating-reviews">
-                        <div className="star-avg-rating">
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                        </div>
-
-                        <div className="reviews-count">
-                          # Review (placeholder)
-                        </div>
-                      </div>
-
-                      <div className="rating-details">
-                        <Link to={`/businesses/${business.id}`}>
-                          <i className="fas fa-signal"></i>
-                          Details
-                        </Link>
-                      </div>
-                    </div>
-
-                    <div className="price-category">
-                      <div className="price">
-                        $$ • <br/>
-                      </div>
-
-                      <div className="business-category">
-                        Catgories!!!
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="top-header-right">
-                  <div className="business-page-actions">
-                    <a className="business-write-review" href="#">
-                      <i className="fas fa-star"></i>
-                      <div className="write-review-btn-text">Write a Review</div>
-                    </a>
-
-                    <div className="page-options">
-                      <Link to={`/businesses/${business.id}`}>
-                        <i className="fas fa-camera"></i>
-                        Add Photo
-                      </Link>
-
-                      <Link to={`/businesses/${business.id}`}>
-                        <i className="fas fa-share-square"></i>
-                        Share
-                      </Link>
-
-                      <div className="bookmark">
-                        <i className="fas fa-bookmark"></i>
-                        Save
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
+        <BusinessPageHeaderContainer props={this.props} />
       </div>
     )
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  const businessId = ownProps.match.params.businessId;
-  const business = state.entities.businesses[businessId];
-
-  return {
-    business,
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchBusinesses: () => dispatch(fetchBusinesses()),
-    fetchBusiness: (id) => dispatch(fetchBusiness(id)),
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(BusinessPage);
+export default BusinessPage;
---
+++ frontend/components/business_page/business_page_header_container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import BusinessPageHeader from './business_page_header';
+import { fetchBusiness, fetchBusinesses } from '../../actions/business_actions';
+
+const mapStateToProps = (state, ownProps) => {
+  const businessId = ownProps.props.match.params.businessId;
+  const business = state.entities.businesses[businessId];
+
+  return {
+    business,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchBusinesses: () => dispatch(fetchBusinesses()),
+    fetchBusiness: (id) => dispatch(fetchBusiness(id)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BusinessPageHeader);
