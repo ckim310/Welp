c07e44c6d802f265949e8d11043ee84c63538318
Christine Kim <ckim31093@gmail.com>
2019-01-28 17:33:45 -0500

    Update sub nav bar to have content

--- frontend/components/nav_bar/sub_nav_bar.jsx
+++ frontend/components/nav_bar/sub_nav_bar.jsx
@@ -9,7 +9,16 @@ const SubNavBar = (props) => {
             <div className="sub-item" id="restaurants">
               <ul className="restaurants-list">
                 <li>
-                  <a href="#"><i className="fas fa-utensils"></i>Restaurants</a>
+                  <a href="#">
+                    <div className="list-icon">
+                      <i className="fas fa-utensils"></i>
+                    </div>
+                    <span>Restaurants</span>
+                    <div className="arrow">
+                      <i className="fas fa-angle-down"></i>
+                    </div>
+                  </a>
+
                   <ul className="popup">
                     <li>
                       <i className="fas fa-motorcycle"></i>
@@ -27,22 +36,145 @@ const SubNavBar = (props) => {
                     </li>
                   </ul>
                 </li>
-
-
               </ul>
             </div>
             
-            <div className="sub-item" id="home-services"></div>
+            <div className="sub-item" id="home-services">
+              <ul className="list">
+                <li>
+                  <a href="#">
+                    <div className="list-icon">
+                      <i className="fas fa-home"></i>
+                    </div>
+                    <span>Home Services</span>
+                    <div className="arrow">
+                      <i className="fas fa-angle-down"></i>
+                    </div>
+                  </a>
 
-            <div className="sub-item" id="auto-services"></div>
+                  <ul className="popup">
+                    <li>
+                      <i className="fas fa-hammer"></i>
+                      <span>Contractors</span>
+                    </li>
 
-            <div className="sub-item" id="more"></div>
+                    <li>
+                      <i className="fas fa-plug"></i>
+                      <span>Electricians</span>
+                    </li>
+                  
+                    <li>
+                      <i className="fas fa-broom"></i>
+                      <span>Home Cleaners</span>
+                    </li>
+                  
+                    <li>
+                      <i className="fas fa-thermometer-half"></i>
+                      <span>HVAC</span>
+                    </li>
+                  
+                    <li>
+                      <i className="fas fa-tree"></i>
+                      <span>Landscaping</span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
 
-            <div className="sub-item" id="empty"></div>
+            <div className="sub-item" id="auto-services">
+              <ul className="list">
+                <li>
+                  <a href="#">
+                    <div className="list-icon">
+                      <i className="fas fa-car"></i>
+                    </div>
+                    <span>Auto Services</span>
+                    <div className="arrow">
+                      <i className="fas fa-angle-down"></i>
+                    </div>
+                  </a>
 
-            <div className="sub-item" id="review"></div>
+                  <ul className="popup">
+                    <li>
+                      <i className="fas fa-oil-can"></i>
+                      <span>Oil Change</span>
+                    </li>
 
-            <div className="sub-item" id="for-business"></div>
+                    <li>
+                      <i className="fas fa-parking"></i>
+                      <span>Parking</span>
+                    </li>
+
+                    <li>
+                      <i className="fas fa-truck-pickup"></i>
+                      <span>Towing</span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+
+            <div className="sub-item" id="more">
+              <ul className="list">
+                <li>
+                  <a href="#">
+                    <span>More</span>
+                    <div className="arrow">
+                      <i className="fas fa-angle-down"></i>
+                    </div>
+                  </a>
+
+                  <ul className="popup">
+                    <li>
+                      <i className="fas fa-mobile"></i>
+                      <span>Phone Repair</span>
+                    </li>
+
+                    <li>
+                      <i className="fas fa-beer"></i>
+                      <span>Bars</span>
+                    </li>
+
+                    <li>
+                      <i className="fas fa-glass-martini"></i>
+                      <span>Nightlife</span>
+                    </li>
+
+                    <li>
+                      <i className="fas fa-cut"></i>
+                      <span>Hair Salons</span>
+                    </li>
+
+                    <li>
+                      <i className="fas fa-dumbbell"></i>
+                      <span>Gyms</span>
+                    </li>
+
+                    <li>
+                      <i className="fas fa-shopping-bag"></i>
+                      <span>Shopping</span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+
+            <div className="sub-item-empty" id="empty"></div>
+
+            <div className="sub-item" id="review">
+              <div className="sub-item-container">
+                <i className="fas fa-pencil-alt"></i>
+                <span>Write a Review</span>
+              </div>
+            </div>
+
+            <div className="sub-item" id="for-business">
+              <div className="sub-item-container">
+                <i className="fas fa-store"></i>
+                <span>For Businesses</span>
+              </div>
+            </div>
 
           </div>
         </div>
