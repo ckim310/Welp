a2f05bedfdf3e413307d40681973a29ee9f01523
Christine Kim <ckim31093@gmail.com>
2019-01-27 22:23:07 -0500

    Add search nav bar to business index and business page

--- frontend/components/business_page/business_page.jsx
+++ frontend/components/business_page/business_page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import BusinessPageHeaderContainer from './business_page_header_container';
 import { fetchBusinesses } from '../../util/business_api_util';
+import SearchNavBar from '../nav_bar/search_nav_bar';
 
 class BusinessPage extends React.Component {
   constructor(props) {
@@ -11,6 +12,7 @@ class BusinessPage extends React.Component {
   render() {
     return (
       <div className="business-page-container">
+        <SearchNavBar />
         <BusinessPageHeaderContainer props={this.props} />
       </div>
     )
--- frontend/components/nav_bar/search_nav_bar.jsx
+++ frontend/components/nav_bar/search_nav_bar.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import NavBarRightContainer from './nav_bar_right_container';
+import SearchForm from '../home_page/search_form';
+import HeaderForm from '../session_form/header_form';
 
 const SearchNavBar = props => {
 
   return (
     <div className="search-nav-bar-container">
-      <NavBarRightContainer />
+      <div className="search-nav-bar-content">
+        <div className="search-nav-bar-item">
+          <HeaderForm />
+        </div>
+
+        <div className="search-nav-bar-item">
+          <SearchForm />
+        </div>
+
+        <div className="search-nav-bar-item">
+          <NavBarRightContainer />
+        </div>
+      </div>
     </div>
   );
 };
