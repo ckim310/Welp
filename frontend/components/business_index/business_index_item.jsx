5c8d41e60a2f46086195e10d570d0677a3f4c978
Christine Kim <ckim31093@gmail.com>
2019-01-28 14:20:42 -0500

    Adjust width of business index list and add filler photo

--- frontend/components/business_index/business_index_item.jsx
+++ frontend/components/business_index/business_index_item.jsx
@@ -5,6 +5,9 @@ const BusinessIndexItem = ({ business, idx }) => {
   return (
   <li className="business-index-item">
     <div className="business-index-content-container">
+      <div className="business-index-pic">
+        <img src="./gongcha.png" alt="gongcha"/>
+      </div>
         <h3 className="business-name">{idx}. <Link to={`/businesses/${business.id}`}>{business.name}</Link></h3>
 
       <div className="business-detail">
