71bf1530a57639b72b75d61058ce0846b59a3f70
Christine Kim <ckim31093@gmail.com>
2019-01-31 11:52:16 -0500

    Create review actions in frontend to create reviews

--- frontend/actions/review_actions.js
+++ frontend/actions/review_actions.js
@@ -1,5 +1,20 @@
 import * as ReviewAPIUtil from '../util/review_api_util';
 
-export const CREATE_REVIEW = "CREATE_REVIEW";
+export const RECEIVE_REVIEW = "RECEIVE_REVIEW";
 export const UPDATE_REVIEW = "UPDATE_REVIEW";
 export const DELETE_REVIEW = "DELETE_REVIEW";
+
+const receiveReview = review => {
+  return {
+    type: RECEIVE_REVIEW,
+    review
+  };
+};
+
+export const createReview = (businessId, review) => {
+  return dispatch => {
+    return ReviewAPIUtil.createReview(businessId, review).then(business => {
+      return dispatch(receiveReview(review));
+    });
+  };
+};
