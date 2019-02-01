43407853331e869428c4bcb19dcb56959aca01ae
Christine Kim <ckim31093@gmail.com>
2019-02-01 01:24:52 -0500

    Update background image with image added on window

--- frontend/components/home_page/home_page.jsx
+++ frontend/components/home_page/home_page.jsx
@@ -7,8 +7,16 @@ import BrowseCategory from './browse_category';
 
 const HomePage = (props) => {
 
+  var sectionStyle = {
+    width: "100%",
+    background: "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + window.burgerURL + ")",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
   return (
-    <div className="home-main-container overlay" >
+    <div className="home-main-container overlay" style={sectionStyle} >
       <div className="home-main-top-container">
         <NavBar />
 
