d847a6413cd02c5c58645dc6108875b06b776713
Christine Kim <ckim31093@gmail.com>
2019-02-05 10:02:10 -0500

    Update file to make production ready and remove logger from production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+  return createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
 };
 
 export default configureStore;
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,52 @@
+// const path = require("path");
+// const webpack = require("webpack");
+
+// var plugins = [];
+// var devPlugins = [];
+
+// var prodPlugins = [
+//   new webpack.DefinePlugin({
+//     'process.env': {
+//       'NODE_ENV': JSON.stringify('production')
+//     }
+//   }),
+//   new webpack.optimize.UglifyJsPlugin({
+//     compress: {
+//       warnings: true
+//     }
+//   })
+// ];
+
+// plugins = plugins.concat(
+//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+// );
+
+// module.exports = {
+//   context: __dirname,
+//   entry: "./frontend/welp.jsx",
+//   output: {
+//     path: path.resolve(__dirname, "app", "assets", "javascripts"),
+//     filename: "bundle.js"
+//   },
+//   plugins: plugins,
+//   resolve: {
+//     extensions: [".js", ".jsx", "*"]
+//   },
+//   devtool: "eval-source-map",
+//   module: {
+//     rules: [{
+//       test: /\.(js|jsx)$/,
+//       use: {
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['@babel/preset-env', '@babel/preset-react']
+//         }
+//       },
+//       exclude: /node_modules/
+//     }]
+//   }
+// };
+
 const path = require("path");
 
 module.exports = {
