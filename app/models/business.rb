156ba53aa3bcfe8cd3264085d43be9b20cf5f517
Christine Kim <ckim31093@gmail.com>
2019-02-20 14:19:55 -0500

    Create in bounds method on businesses model

--- app/models/business.rb
+++ app/models/business.rb
@@ -23,4 +23,10 @@ class Business < ApplicationRecord
 
   has_many :bookmarks
 
+  def self.in_bounds(bounds)
+    self.where("lat < ?", bounds[:northEast][:lat])
+      .where("lat > ?", bounds[:southWest][:lat])
+      .where("lng > ?", bounds[:southWest][:lng])
+      .where("lng < ?", bounds[:northEast][:lng])
+  end
 end
