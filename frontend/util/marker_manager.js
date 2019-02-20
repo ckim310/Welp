8df123a167690ec59a1e9d8ead1670040f71e4d5
Christine Kim <ckim31093@gmail.com>
2019-02-20 12:19:49 -0500

    Add marker labels and marker drop animation

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -5,17 +5,20 @@ export default class MarkerManager {
   }
 
   updateMarkers(businesses) {
-    businesses.forEach(business => {
-      this.createMarkerFromBusiness(business);
+    businesses.forEach((business, idx) => {
+      const index = idx + 1;
+      this.createMarkerFromBusiness(business, index);
     });
   }
 
-  createMarkerFromBusiness(business) {
+  createMarkerFromBusiness(business, idx) {
     const position = new google.maps.LatLng(business.latitude, business.longitude);
   
     const marker = new google.maps.Marker({
       position,
       map: this.map,
+      label: `${idx}`,
+      animation: google.maps.Animation.DROP,
       businessId: business.id
     });
   
